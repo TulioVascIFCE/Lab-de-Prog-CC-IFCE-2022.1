6fd75bebd30dd16e790d4b8b19deca4bd8ce7377
TulioVascIFCE <93345434+TulioVascIFCE@users.noreply.github.com>
2022-04-10 11:58:36 -0300

    Update lista1_q21.c

--- Lista_01/lista1_q21.c
+++ Lista_01/lista1_q21.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
+int multiplo(int primeiro, int segundo){
+    return !((primeiro % segundo) == 0);
+}
+
 int main(){
-  int primeiro, segundo, multiplo;
+  int primeiro, segundo;
   
   puts("Entre com um inteiro:");
   scanf("%d", &primeiro);
   puts("Entre com outro inteiro:");
   scanf("%d", &segundo);
 
-  multiplo = (!(primeiro % segundo)) ? 0 : 1;
-
-  printf("O primeiro numero e multiplo do segundo: %d\n(0 multiplo, 1 nao multiplo)\n", multiplo);
+  printf("O primeiro numero e multiplo do segundo: %d\n(0 multiplo, 1 nao multiplo)\n", multiplo(primeiro, segundo));
   
   return (0);
 }
