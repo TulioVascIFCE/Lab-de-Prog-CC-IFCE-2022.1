caa1caf55ada02ac42bbeb72cd3a2ce1dda9a4b1
Túlio de Oliveira Vasconcelos <93345434+TulioVascIFCE@users.noreply.github.com>
2022-05-01 15:34:58 +0000

    lista3_q02 ok

--- Lista_03/lista3_q01.c
+++ Lista_03/lista3_q01.c
@@ -1,60 +1,62 @@
 #include <stdio.h>
+#include <time.h>
 
-#define CENTESIMO 99
+#define TAMANHO 100
 
-float maiorElemento(float *vetor);
-float menorElemento(float *vetor);
+float maiorElemento (float *vetor);
+float menorElemento (float *vetor);
 
-int main(){
+int main (){
 	//Variaveis
-	float vetor[15], soma;
+	float vetor[TAMANHO], soma;
 	
 	//Leitura de dados - Aleatorio
-	puts("Vetor Aleatório:");
-	for(int i = 0; i < 15; i++){
-		*(vetor + i) = (rand() % 1) + (float) (rand() % CENTESIMO) / 101;
-		printf("%.2f\t", *(vetor + i));
+	puts ("Vetor Aleatório:");
+	srand (time (NULL));
+	for (int i = 0; i < TAMANHO; i++){
+		* (vetor + i) = (float) (rand () % 101) / 100;
+		printf ("%.2f  ", * (vetor + i));
 	}
 	
 	//Saida de dados
-	puts("");
-	soma = maiorElemento(vetor) + menorElemento(vetor);
-	printf("Soma: %.2f\n", soma);
+	puts ("");
+	soma = maiorElemento (vetor) + menorElemento (vetor);
+	printf ("Soma: %.2f\n", soma);
 	
 	//Fim do Programa
 	return 0;
 }
 
-float maiorElemento(float *vetor){
+float maiorElemento (float *vetor){
 	float maior, aux;
 	
-	for(int j = 0; j<14; j++){
-		if(*(vetor + j) > *(vetor + j+1)){
-			aux = *(vetor + j);
-			*(vetor + j) = *(vetor + j+1);
-			*(vetor + j+1) = aux;
+	for (int j = 0; j < TAMANHO - 2; j++){
+		if (* (vetor + j) > * (vetor + j + 1)){
+			aux = * (vetor + j);
+			* (vetor + j) = * (vetor + j+1);
+			* (vetor + j + 1) = aux;
 		}
 	}
 	
-	maior = *(vetor + 14);
-	printf("Maior Elemento do Vetor: %.2f\n", maior);
+	maior = * (vetor + TAMANHO - 2);
+	printf ("Maior Elemento do Vetor: %.2f\n", maior);
 
 	return maior;
 }
 
-float menorElemento(float *vetor){
+float menorElemento (float *vetor){
 	float menor, aux;
 	
-	for(int j = 0; j<14; j++){
-		if(*(vetor + j) < *(vetor + j+1)){
-			aux = *(vetor + j);
-			*(vetor + j) = *(vetor + j+1);
-			*(vetor + j+1) = aux;
+	for (int j = 0; j < TAMANHO - 2; j++){
+		if (* (vetor + j) < * (vetor + j + 1)){
+			aux = * (vetor + j);
+			* (vetor + j) = * (vetor + j + 1);
+			* (vetor + j + 1) = aux;
 		}
 	}
 	
-	menor = *(vetor + 14);
-	printf("Menor Elemento do Vetor: %.2f\n", menor);
+	menor = * (vetor + TAMANHO - 2);
+	printf ("Menor Elemento do Vetor: %.2f\n", menor);
 
 	return menor;
 }
---
+++ Lista_03/lista3_q02.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#define TAMANHO 100
+#define ASCII 95
+
+void buscaSentinela (unsigned char *v, char c, int t);
+
+int main (){
+	//Variáveis
+	unsigned char vetor[TAMANHO];
+	char caractere, aux;
+	
+	//Leitura de dados - Aleatório
+	//srand(time(NULL));
+	//for(int i = 0; i < TAMANHO; i++){
+	//	*(vetor + i) = 32 + (rand() % ASCII);
+	//}
+
+	//Leitura de dados - Usuário
+	puts ("Entre com uma strig:");
+	fgets  (vetor, TAMANHO, stdin);
+	if (vetor[strlen (vetor) - 1] == '\n') vetor[strlen (vetor) - 1] = '\0';
+	else {
+		do {
+			aux = getchar ();
+		}while (aux != '\n');
+	}
+	
+	//Valor a ser procurado:
+	puts ("Entre com o caractere que deseja procurar no vetor:");
+	scanf ("%c", &caractere);
+	
+	//Saída de dados
+	puts ("A string gerada foi:");
+	printf ("%s\n", vetor);	
+	buscaSentinela (vetor, caractere, TAMANHO);
+		
+	//Fim do Programa
+	return 0;
+}
+
+void buscaSentinela (unsigned char *v, char c, int t){
+	unsigned char k = 0;
+	char resposta, ultimo;
+
+	ultimo = * (v + t - 1);
+	* (v + t - 1) = c;
+	while(* (v + k) != c){
+		k++;
+	}
+	* (v + t - 1) = ultimo;
+	if (k < (t - 1) || * (v + t - 1) == c){
+		resposta = 'S';
+	}else resposta = 'N';
+	printf ("Valor procurado encontrado: %c\n", resposta);
+	if (resposta == 'S') printf ("Posição: %hhu\n", k);
+}
