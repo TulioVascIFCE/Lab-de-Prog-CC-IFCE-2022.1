3661eb879eeea28ca0f0f22a2d7dc57dd74e1f3c
Túlio de Oliveira Vasconcelos <93345434+TulioVascIFCE@users.noreply.github.com>
2022-05-23 18:08:31 +0000

    lista4q4 ok

--- Lista_04/lista4_q04.c
+++ Lista_04/lista4_q04.c
@@ -3,33 +3,30 @@
 #define TAMANHO 3
 
 int main () {
-	//char caractere [TAMANHO], *pc;
+	char caractere [TAMANHO], *pc;
 	int inteiro[TAMANHO], *pi;
 	float ponto_flutuante[TAMANHO], *pf;
 
-	//pc = caractere;
+	pc = caractere;
 	pi = inteiro;
 	pf = ponto_flutuante;
 
-	for (int k; k < TAMANHO; k++) {
-		//puts ("Entre com um caractere:");
-		//scanf ("%c", pc+k);
+	for (int k = 0; k < TAMANHO; k++) {
+		puts ("Entre com um caractere:");
+		scanf ("%c", pc + k);
 		puts ("Entre com um inteiro:");
-		scanf ("%d", pi+k);
+		scanf ("%d", pi + k);
 		puts ("Entre com um ponto flutuante:");
-		scanf ("%f", pf+k);
+		scanf ("%f", pf + k);
+		getchar();
 	}
 
 	puts ("Vetores e Endereços:");
-	//printf ("Caractere\tEndereço\n");
-	//for (int k; k < TAMANHO; k++) printf ("%c\t%p\n", *(pc+k), pc+k);
+	printf ("Caractere\tEndereço\n");
+	for (int k = 0; k < TAMANHO; k++) printf ("%c\t%p\n", *(pc +  k), pc + k);
 	printf ("Inteiro\tEndereço\n");
-	for (int k; k < TAMANHO; k++) {
-		printf ("%d\t%p\n", *(pi+k), pi+k);
-		}
+	for (int k = 0; k < TAMANHO; k++) printf ("%d\t%p\n", *(pi + k), pi + k);
 	printf ("Ponto Flutuante\tEndereço\n");
-	for (int k; k < TAMANHO; k++) {
-		printf ("%.2f\t%p\n", *(pf+k), pf+k);
-		}
+	for (int k = 0; k < TAMANHO; k++) printf ("%.2f\t%p\n", *(pf + k), pf + k);
 	return 0;
 }
