48c77f28b60a067d959874653cb4d4f146fdcc77
Túlio de Oliveira Vasconcelos <93345434+TulioVascIFCE@users.noreply.github.com>
2022-05-23 17:49:39 +0000

    lista4q4 +-

--- Lista_04/lista4_q04.c
+++ Lista_04/lista4_q04.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
-int main(){
-	
-	
+#define TAMANHO 3
+
+int main () {
+	//char caractere [TAMANHO], *pc;
+	int inteiro[TAMANHO], *pi;
+	float ponto_flutuante[TAMANHO], *pf;
+
+	//pc = caractere;
+	pi = inteiro;
+	pf = ponto_flutuante;
+
+	for (int k; k < TAMANHO; k++) {
+		//puts ("Entre com um caractere:");
+		//scanf ("%c", pc+k);
+		puts ("Entre com um inteiro:");
+		scanf ("%d", pi+k);
+		puts ("Entre com um ponto flutuante:");
+		scanf ("%f", pf+k);
+	}
+
+	puts ("Vetores e Endereços:");
+	//printf ("Caractere\tEndereço\n");
+	//for (int k; k < TAMANHO; k++) printf ("%c\t%p\n", *(pc+k), pc+k);
+	printf ("Inteiro\tEndereço\n");
+	for (int k; k < TAMANHO; k++) {
+		printf ("%d\t%p\n", *(pi+k), pi+k);
+		}
+	printf ("Ponto Flutuante\tEndereço\n");
+	for (int k; k < TAMANHO; k++) {
+		printf ("%.2f\t%p\n", *(pf+k), pf+k);
+		}
 	return 0;
 }
