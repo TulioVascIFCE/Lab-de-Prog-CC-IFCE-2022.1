dd9148fe9d5918a642b335926b1aaf7efa574998
Túlio de Oliveira Vasconcelos <93345434+TulioVascIFCE@users.noreply.github.com>
2022-05-24 20:02:27 +0000

    lista4q12 ok

--- Lista_04/lista4_q12.c
+++ Lista_04/lista4_q12.c
@@ -40,48 +40,6 @@ int main () {
 	}
 	for (int k = 0; k < (intervalo * intervalo); k++)
 		*(matriz_M + k) = 0;
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-void gerandoY (int *, int *, int , int );
-void algoritmo_rol (int *, int *, int *, int , int);
-
-int main () {
-	int *vetor_X, *vetor_Y, *matriz_M;
-	int vTam, intervalo;
-
-	puts("Qual tamanho dos vetores X e Y?");
-	scanf("%d", &vTam);
-	
-	vetor_X = malloc(vTam * sizeof (int));
-	if (!vetor_X) {
-		puts("Falha na solitação de memória!");
-		exit(1);
-	}
-	
-	vetor_Y = malloc(vTam * sizeof (int));
-	if (!vetor_Y) {
-		puts("Falha na solitação de memória!");
-		exit(1);
-	}
-
-	puts("Qual o limite N do intervalo? (0 até N-1)");
-	scanf("%d", &intervalo);
-	
-	//srand (time (NULL));
-	for (int k = 0; k < vTam; k++)
-		*(vetor_X + k) = rand () % intervalo;
-
-	gerandoY(vetor_X, vetor_Y, vTam, intervalo);
-
-	matriz_M = malloc((intervalo * intervalo) * sizeof(int));
-	if (!matriz_M) {
-		puts("Falha na solitação de memória!");
-		exit(1);
-	}
-	for (int k = 0; k < (intervalo * intervalo); k++)
-		*(matriz_M + k) = 0;
 
 	algoritmo_rol (vetor_X, vetor_Y, matriz_M, vTam, intervalo);
 
