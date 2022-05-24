000536faa6f0de2d0f5c8131ca0484d20eae544f
Túlio de Oliveira Vasconcelos <93345434+TulioVascIFCE@users.noreply.github.com>
2022-05-24 00:35:30 +0000

    lista4q8 ok

--- Lista_04/lista4_q08.c
+++ Lista_04/lista4_q08.c
@@ -10,24 +10,28 @@ int main () {
 	srand(time(NULL));
 	
 	//Arranjo aleatório:
-	for (int i = 0; i < TAMANHO; i++){
+	for (int i = 0; i < TAMANHO; i++)
 		*(arranjo + i) = ' ' + (rand() & 94);
-	}
 	
 	//Valor a ser procurado:
 	puts("Entre com o valor de X:");
 	scanf ("%c", &x);
 
 	//Busca com "Sentinela":
+	
 	ultimo = *(arranjo + TAMANHO);
 	*(arranjo + TAMANHO) = x;
+	
 	while (*(arranjo + indice_resposta) != x)
 		indice_resposta ++;
 	*(arranjo + TAMANHO) = ultimo;
+	
 	if (indice_resposta < TAMANHO || *(arranjo + TAMANHO) == x) {
 		resposta = 'S';
 	} else resposta = 'N';
+	
 	printf ("Valor procurado encontrado %c\n", resposta);
+	
 	if (resposta == 'S')
 		printf ("Posição: %d\n", indice_resposta);
 
