241cca0d40d6e8060394531d0ffcd4c9644de9ab
Túlio de Oliveira Vasconcelos <93345434+TulioVascIFCE@users.noreply.github.com>
2022-07-04 19:48:24 +0000

    tf wip

--- teste.c
+++ teste.c
@@ -5,13 +5,14 @@
 
 //Protótipos da funções
 void aleatoria(int *, int*);
+void matrizDentroMatriz(int *, int *, int *);
+void lbp(int *, int *, int *);
 //void binaria(int *, int *, int *);
-void lbp(int *, int *, int *, int *);
 void exibirMatriz(int *, int*);
 
 int main(int argc, char *argv[]){
 	//Declaração de Variáveis
-	int vetor[5], *matriz_S, *matriz_M, *matriz_Aux, aux;
+	int vetor[7], *matriz_S, *matriz_M, *matriz_Aux, aux;
 
 	//Recebendo os argumentos de entrada
 	if (argc != 5){
@@ -24,7 +25,7 @@ int main(int argc, char *argv[]){
 		*(vetor + (aux-1)) = atoi(*(argv + aux));
 		aux++;
 	}
-
+		
 	if (*(vetor + 2) > *(vetor + 3)){
 		aux = *(vetor + 2);
 		*(vetor + 2) = *(vetor + 3);
@@ -37,10 +38,13 @@ int main(int argc, char *argv[]){
 		exit(2);
 	}
 
+	*(vetor + 5) = *vetor + 2;
+	*(vetor + 6) = *(vetor + 1) + 2;
+
 	//Gerando a Matriz normal M e a binária S
 	matriz_M = malloc((*vetor * *(vetor + 1)) * sizeof(int));
 	matriz_S = calloc((*vetor * *(vetor + 1)), sizeof(int));
-	matriz_Aux = calloc(((*vetor + 1) * (*(vetor + 1) + 1)), sizeof(int));
+	matriz_Aux = calloc(*(vetor + 5) * *(vetor + 6), sizeof(int));
 
 	//Validando as Matrizes dadas
 	if (!matriz_M || !matriz_S || !matriz_Aux){
@@ -53,20 +57,24 @@ int main(int argc, char *argv[]){
 
 	aleatoria(matriz_M, vetor);
 
-	//binaria(matriz_M, matriz_S, vetor);
-	lbp(matriz_M, matriz_S, matriz_Aux, vetor);
+	matrizDentroMatriz(matriz_M, matriz_Aux, vetor);
+	lbp(matriz_Aux, matriz_S, vetor);
 	
 	//Exibindo os dados
-	puts("Matriz M (Original):");
-	exibirMatriz(matriz_M, vetor);
+	//puts("Matriz M (Original):");
+	//exibirMatriz(matriz_M, vetor);
 
-	puts("Matriz S (Binária):");
-	exibirMatriz(matriz_S, vetor);
+	//puts("Matriz Aux (Auxiliar):");
+	//exibirMatriz(matriz_Aux, vetor + 5);
+
+	//puts("Matriz S (Binária):");
+	//exibirMatriz(matriz_S, vetor);
 
 	//Liberando a Memória e Encerrando programa
 	puts("");
 	free(matriz_M);
 	free(matriz_S);
+	free(matriz_Aux);
 	
 	return EXIT_SUCCESS;
 }
@@ -82,28 +90,34 @@ void aleatoria(int *A, int *lim){
 	for( int k = 0; k < (*lim * *(lim + 1)); k++ ) *(A + k) = *(lim + 2) + (rand() % (*(lim + 3) + 1));
 }
 
-void lbp(int *A, int *B, int *C, int *v){
-	int subMatriz[9], pesos[8];
+void matrizDentroMatriz(int *A, int *B, int *v){
+	int aux = 0;
+	for( int k = 0; k < *v * *(v + 1); k++ ){
+		aux += 1;
+		if (k % *(v + 1) == 0 && k != 0) aux += 2;
+		*(B + *(v + 6) + aux) = *(A + k);
+	}
+}
+
+void lbp(int *A, int *B, int *v){
+	int subMatriz[9];
 	int aux[3] = {-1, 0, 1};
-	for( int k = 0; k < 16; k++){
-		printf("\nSub Matriz %d:", k);
+	for( int k = *(v + 6) + 1; k < *(v + 5) * *(v + 6) - *(v + 6); k++){
+		if( k % *(v + 6) == 0 || k % *(v + 6) == *(v + 6) - 1 ) continue;
+		//printf("\nSub Matriz %d:", k);
 		for( int i = 0; i < 3; i++){
 			for( int j = 0; j < 3; j++){
-				if( A + *(v +1)*aux[i] + aux[j] + k < A || A + *(v +1)*aux[i] + aux[j] + k > A + (*v * *(v + 1)))
-					*(subMatriz + 3*i + j) = 0;
-				else
-					*(subMatriz + 3*i + j) = *(A + *(v +1)*aux[i] + aux[j] + k);
-				if( !(j%3) ) printf("\n");
-				printf("%d\t", *(subMatriz + 3*i + j));
+				*(subMatriz + 3*i + j) = *(A + *(v + 5)*aux[i] + aux[j] + k);
+				//if( !(j%3) ) printf("\n");
+				//printf("%d\t", *(subMatriz + 3*i + j));
 				
 			}
 		}
 	}
-	puts("");
+	//puts("");
 }
-/*
+
 void binaria(int *A, int *B, int *lim){
 	for (int k = 0; k < (*lim * *(lim + 1)); k++)
 		if (*(A + k) > *(lim + 4)) *(B + k) = 1;
 }
-*/
